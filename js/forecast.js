7629dac904f4533130eee57e48dac15e5ddf75bc
rodrigo blejman <rodrigo.blej@gmail.com>
2022-05-30 17:32:01 -0300

    form setup

--- js/forecast.js
+++ js/forecast.js
@@ -1,5 +1,15 @@
 const key = "1Upr5HKNJr1IeVupx3RirMbnkR2gDwsD";
 
+//City weather conditions
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+// City info
 const getCity = async (city) => {
   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
@@ -7,7 +17,3 @@ const getCity = async (city) => {
   const data = await response.json();
   return data[0];
 };
-
-getCity("manchester")
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
