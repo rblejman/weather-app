b1e34ec2034ea6aa2c07db37149c0cb78b5eac68
rodrigo blejman <rodrigo.blej@gmail.com>
2022-05-31 15:23:05 -0300

    icons img added

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector("img .time");
+const icon = document.querySelector(".icon img");
 
 const updateUI = (data) => {
-  const cityDets = data.cityDets;
-  const weather = data.weather;
+  const { cityDets, weather } = data;
 
   details.innerHTML = `
   <h5 class="my-3">${cityDets.EnglishName}</h5>
